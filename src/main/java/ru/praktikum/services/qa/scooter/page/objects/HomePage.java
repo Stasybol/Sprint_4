045a71c91d9377ec45ea337a94b19824914d8987
Stasy Bolotnikova <stasybol95@gmail.com>
2024-12-23 16:09:42 +0300

    Objets page home

--- src/main/java/ru/praktikum/services/qa/scooter/page/objects/HomePage.java
+++ src/main/java/ru/praktikum/services/qa/scooter/page/objects/HomePage.java
@@ -1,12 +1,9 @@
 package ru.praktikum.services.qa.scooter.page.objects;
 
-import dev.failsafe.internal.util.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.JavascriptExecutor;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
